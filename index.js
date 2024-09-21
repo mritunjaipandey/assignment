ddd78e68830f1483fb66aa1eefd8ee4c75e278eb
Mritunjai Pandey <149992631+mritunjaipandey@users.noreply.github.com>
2024-09-21 14:31:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle-board').addEventListener('click', function() {
+    var board = document.getElementById('journey-board');
+    board.classList.toggle('collapsed');
+  });
